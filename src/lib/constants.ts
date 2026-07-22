0765a45812746f300dd33f5ea7e1225c08b4d92b
Noble Company Digital <noblecompany.digital@gmail.com>
2026-07-22 05:48:24 -0300

    Atualizações de favicon, ajuste na imagem do ivo e alteração de endereço de agendamento

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,1 @@
-export const BOOKING_URL = "https://www.noona.pt/secretlooklisboa";
+export const BOOKING_URL = "https://wa.me/351929075519?text=Ol%C3%A1%21%20Vim%20atrav%C3%A9s%20do%20site%20e%20gostaria%20de%20marcar%20um%20hor%C3%A1rio";
